54d9ddd4f930311871d8b1e0cdbf2fdcce948270
Cho sungbok <gging200@gradehc.com>
2023-06-09 13:46:24 +0900

    add package.swfit

--- Package.swift
+++ Package.swift
@@ -2,4 +2,16 @@ import PackageDescription
 
 let package = Package(
     name: "OpalImagePicker"
+    products: [
+        .library(
+            name: "OpalImagePicker"
+            targets: ["OpalImagePicker"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "OpalImagePicker"
+            path: "OpalImagePicker/Source"
+        )
+    ]
 )
